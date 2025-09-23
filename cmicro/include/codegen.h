8d1961334b51792c63af22f8f047d55d0c7864fb
Kevin Alavik <kevin@piraterna.org>
2025-09-23 11:32:41 +0200

    feat: Added codegen template files, prepare for QBE

---
+++ cmicro/include/codegen.h
@@ -0,0 +1,16 @@
+/*
+ * cmicro - Micro compiler
+ * Copyright (C) 2025 Kevin Alavik <kevin@alavik.se>
+ *
+ * Licensed under the Apache License, Version 2.0
+ */
+
+#ifndef _CMICRO_CODEGEN_H
+#define _CMICRO_CODEGEN_H
+
+#include <parser.h>
+#include <stdio.h>
+
+int codegen_generate(ast_node_t* root, FILE* out);
+
+#endif // _CMICRO_CODEGEN_H
---
+++ cmicro/src/codegen.c
@@ -0,0 +1,15 @@
+/*
+ * cmicro - Micro compiler
+ * Copyright (C) 2025 Kevin Alavik <kevin@alavik.se>
+ *
+ * Licensed under the Apache License, Version 2.0
+ */
+
+#include <codegen.h>
+
+int codegen_generate(ast_node_t* root, FILE* out)
+{
+    (void) root;
+    (void) out;
+    /* TODO */
+}
